f5fdad77ad4c994d1c5ccb6c31f83ed95b7d09f7
Jeroen Nijhuis <jeroen@nijhuis.online>
2023-10-20 12:19:22 +0200

    overhauled clock script, removed unused versions

--- BTTF_LAMP_CLOCK_WEBPORTAL.ino
+++ BTTF_LAMP_CLOCK_WEBPORTAL.ino
@@ -1,193 +1,310 @@
-
+#include "FS.h"
 #include "Adafruit_NeoPixel.h"
 #include "TM1637Display.h"
 #include "WiFiManager.h"
 #include "NTPClient.h"
+#include "ArduinoJson.h"
 
+#ifdef ESP32
+  #include "SPIFFS.h"
+#endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        5 // Strip led DIN
-
-#define red_CLK 16
-#define red1_DIO 17
-#define red2_DIO 18
-#define red3_DIO 19
-
+#define ANALOG_SWITCH_PIN 34
+#define LED_PIN 5
+#define RED_CLK 16
+#define RED1_DIO 17
+#define RED2_DIO 18
+#define RED3_DIO 19
 #define AM 32
 #define PM 33
+#define NUMPIXELS 48
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 48 // Popular NeoPixel ring size
+int LED_COLOR_MODE = 0;
+const long UTC_OFFSET_SECONDS = 3600;
+int UTC_OFFSET_HOURS = 2;
+const int DISPLAY_BRIGHTNESS = 3;
+const int LED_BRIGHTNESS = 250;
 
-bool res;
-int var=0;
-int analogPin = 34;
-const long utcOffsetInSeconds = 3600; // Offset in second
+const int SWITCH_HOLD_TIME = 3000;
+unsigned long switchPressStartTime = 0;
 
-//========================USEFUL VARIABLES=============================
-int UTC = 2; // UTC + value in hour - Summer time
-int Display_backlight = 3; // Set displays brightness 0 to 7;
-//======================================================================
+const char* AP_NAME = "BTTF_LAMP_CLOCK";
+const char* AP_PASS = "password";
 
-// When setting up the NeoPixel library, we tell it how many pixels,
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-// Setup the red displays
-TM1637Display red1(red_CLK, red1_DIO);
-TM1637Display red2(red_CLK, red2_DIO);
-TM1637Display red3(red_CLK, red3_DIO);
-
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+TM1637Display red1(RED_CLK, RED1_DIO);
+TM1637Display red2(RED_CLK, RED2_DIO);
+TM1637Display red3(RED_CLK, RED3_DIO);
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds*UTC);
+NTPClient timeClient(ntpUDP, "pool.ntp.org", UTC_OFFSET_SECONDS * UTC_OFFSET_HOURS);
+WiFiManager manager;       
 
 void setup() {
-
-  pinMode(PIN, OUTPUT);
-  pinMode(red_CLK, OUTPUT);
-  pinMode(red1_DIO, OUTPUT);
-  pinMode(red2_DIO, OUTPUT);
-  pinMode(red3_DIO, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(RED_CLK, OUTPUT);
+  pinMode(RED1_DIO, OUTPUT);
+  pinMode(RED2_DIO, OUTPUT);
+  pinMode(RED3_DIO, OUTPUT);
   pinMode(AM, OUTPUT);
   pinMode(PM, OUTPUT);
 
-  pinMode(analogPin, INPUT);
-  //manager.resetSettings();
+  pinMode(ANALOG_SWITCH_PIN, INPUT);
 
   Serial.begin(9600);
-  
-    WiFiManager manager;    
-     
-   manager.setTimeout(180);
-  //fetches ssid and password and tries to connect, if connections succeeds it starts an access point with the name called "BTTF_CLOCK" and waits in a blocking loop for configuration
-  res = manager.autoConnect("BTTF_LAMP_CLOCK","password");
-  
-  if(!res) {
-  Serial.println("failed to connect and timeout occurred");
-  ESP.restart(); //reset and try again
-  }
-  
-  delay(3000);
 
-  timeClient.begin();
-  red1.setBrightness(Display_backlight);
-  red2.setBrightness(Display_backlight);
-  red3.setBrightness(Display_backlight);
-  pixels.setBrightness(250);
-  
+  loadConfig();
+  setupWifiManager();
+  setupTimeClient();
+  setupBrightness();
 }
 
 void loop() {
+  updateTimeDisplays();
+  updateAMPMLeds();  
+  updateBacklight();
+}
 
+void loadConfig() {
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+
+        #if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
+        DynamicJsonDocument json(1024);
+        auto deserializeError = deserializeJson(json, buf.get());
+        serializeJson(json, Serial);
+        if ( ! deserializeError ) {
+        #else
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+        #endif
+
+          Serial.println("\nparsed json");
+          UTC_OFFSET_HOURS = json["utc_offset"];
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+}
+
+bool shouldSaveConfig = false;
+void setShouldSaveConfig() {
+  shouldSaveConfig = true;
+}
+
+void storeConfig() {
+  if (SPIFFS.format()) {
+    Serial.println("Successfully formatted FS");
+  }
+
+  if (SPIFFS.begin()) {
+      Serial.println("mounted file system");
+  } else {
+    Serial.println("failed to mount fs");
+  }
+
+  Serial.println("saving config");
+ #if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
+  DynamicJsonDocument json(1024);
+#else
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+#endif
+  json["utc_offset"] = UTC_OFFSET_HOURS;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    Serial.println("failed to open config file for writing");
+  }
+
+#if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
+  serializeJson(json, Serial);
+  serializeJson(json, configFile);
+#else
+  json.printTo(Serial);
+  json.printTo(configFile);
+#endif
+  configFile.close();
+}
+
+void setupWifiManager() {
+  WiFiManagerParameter custom_utc_offset("utf_offset", "UTC Offset", String(UTC_OFFSET_HOURS).c_str(), 2);
+
+  manager.setDarkMode(true);
+  manager.addParameter(&custom_utc_offset);
+  manager.setSaveConfigCallback(setShouldSaveConfig);
+  manager.setTimeout(180);
+
+  bool wifiResult = manager.autoConnect(AP_NAME, AP_PASS);
+  
+  if (!wifiResult) {
+    ESP.restart();
+  }
+
+  if (shouldSaveConfig) {
+    UTC_OFFSET_HOURS = atoi(custom_utc_offset.getValue());
+    storeConfig();
+  }
+  
+  delay(3000);
+}
+
+void setupTimeClient() {
+  timeClient.begin();
+}
+
+void setupBrightness() {
+  red1.setBrightness(DISPLAY_BRIGHTNESS);
+  red2.setBrightness(DISPLAY_BRIGHTNESS);
+  red3.setBrightness(DISPLAY_BRIGHTNESS);
+  pixels.setBrightness(LED_BRIGHTNESS);
+}
+
+void updateTimeDisplays() {
   timeClient.update();
-  Serial.print("Time: ");
-  Serial.println(timeClient.getFormattedTime());
   unsigned long epochTime = timeClient.getEpochTime();
   struct tm *ptm = gmtime ((time_t *)&epochTime); 
   int currentYear = ptm->tm_year+1900;
-  Serial.print("Year: ");
-  Serial.println(currentYear);
-  
   int monthDay = ptm->tm_mday;
-  Serial.print("Month day: ");
-  Serial.println(monthDay);
-
   int currentMonth = ptm->tm_mon+1;
-  Serial.print("Month: ");
-  Serial.println(currentMonth);
   
-red1.showNumberDecEx(currentMonth,0b01000000,true,2,0);
-red1.showNumberDecEx(monthDay,0b01000000,true,2,2);
-red2.showNumberDecEx(currentYear,0b00000000,true);
-red3.showNumberDecEx(timeClient.getHours(),0b01000000,true,2,0);
-red3.showNumberDecEx(timeClient.getMinutes(),0b01000000,true,2,2);
+  red1.showNumberDecEx((currentMonth * 100) + monthDay, 0b01000000, true);
+  red2.showNumberDecEx(currentYear, 0b00000000, true);
+  red3.showNumberDecEx((timeClient.getHours() * 100) + timeClient.getMinutes(), 0b01000000, true);
 
-if((currentMonth*30 + monthDay) >= 121 && (currentMonth*30 + monthDay) < 331){
-timeClient.setTimeOffset(utcOffsetInSeconds*UTC);} // Change daylight saving time - Summer
-else {timeClient.setTimeOffset((utcOffsetInSeconds*UTC) - 3600);} // Change daylight saving time - Winter
-
-
-if(timeClient.getHours()>=13){
-  digitalWrite(AM,0);
-  digitalWrite(PM,1);}
-  
-else if(timeClient.getHours()==12){
-  digitalWrite(AM,0);
-  digitalWrite(PM,1);}
-
-else{
-  digitalWrite(AM,1);
-  digitalWrite(PM,0);}
-
-
-  pixels.clear(); // Set all pixel colors to 'off'
-  
-  if(var>3){var = 0;} // Reset counter
-
-// read the switch
-  if(analogRead(analogPin) > 100)
-  { 
-    var = var+1 ;
-    delay(45);
+  if((currentMonth * 30 + monthDay) >= 121 && (currentMonth * 30 + monthDay) < 331) {
+    // DST SUMMER
+    timeClient.setTimeOffset(UTC_OFFSET_SECONDS * UTC_OFFSET_HOURS);}
+  else {
+    // DST WINTER
+    timeClient.setTimeOffset((UTC_OFFSET_SECONDS * UTC_OFFSET_HOURS) - 3600);
   }
-
-Serial.print("Var=");
-Serial.println(var);
-Serial.print("Digital=");
-Serial.println(analogRead(analogPin));
-
-delay(100);
-  switch (var) {
-  case 0: 
-  for(int i=0; i<6;i++){
-  pixels.setPixelColor(i, pixels.Color(255,0,0));
-  }
-  for(int i=6; i<12;i++){
-  pixels.setPixelColor(i, pixels.Color(160,160,0));
-  }
-  for(int i=12; i<18;i++){
-  pixels.setPixelColor(i, pixels.Color(255,0,0));
-  }
-  pixels.show();
-  break;
-
-  case 1:
-    for(int i=0; i<6;i++){
-  pixels.setPixelColor(i, pixels.Color(0,0,255));
-  }
-  for(int i=6; i<12;i++){
-  pixels.setPixelColor(i, pixels.Color(200,250,255));
-
-  }
-  for(int i=12; i<18;i++){
-  pixels.setPixelColor(i, pixels.Color(0,0,255));
-  }
- pixels.show();
- break;
-
- case 2:
-  pixels.clear();
-  for(int i=0; i<6;i++){
-  pixels.setPixelColor(i, pixels.Color(255,0,10));
-  }
-    for(int i=6; i<12;i++){
-  pixels.setPixelColor(i, pixels.Color(0,10,255));
-  }
-    for(int i=12; i<18;i++){
-  pixels.setPixelColor(i, pixels.Color(255,0,10));
-  }
- pixels.show();
- break;
-
-case 3:
-  pixels.clear();
-  for(int i=0; i<16;i++){
-  pixels.setPixelColor(i, pixels.Color(0,0,0));
-  }
- pixels.show();
- break;
-
-  }
-
-
-
 }
 
+void updateAMPMLeds() {
+  if(timeClient.getHours() >= 13) {
+    digitalWrite(AM, 0);
+    digitalWrite(PM, 1);
+  } else if(timeClient.getHours() == 12) {
+    digitalWrite(AM, 0);
+    digitalWrite(PM, 1);
+  } else{
+    digitalWrite(AM, 1);
+    digitalWrite(PM, 0);
+  }
+}
+
+void updateBacklight() {
+  pixels.clear(); 
+
+  if(LED_COLOR_MODE > 3){ LED_COLOR_MODE = 0; } // Reset counter
+
+  handleSwitchPress();
+  
+  switch (LED_COLOR_MODE) {
+    case 0: 
+      for(int i = 0; i < 14; i++) {
+        pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+      }
+      for(int i = 14; i < 29; i++) {
+        pixels.setPixelColor(i, pixels.Color(160, 160, 0));
+      }
+      for(int i = 29; i < 44; i++) {
+        pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+      }
+    break;
+
+    case 1:
+      for(int i = 0; i < 14; i++) {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+      }
+      for(int i = 14; i < 29; i++) {
+        pixels.setPixelColor(i, pixels.Color(200, 250, 255));
+      }
+      for(int i = 29; i < 44; i++) {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+      }
+    break;
+
+    case 2:
+      for(int i = 0; i < 14; i++) {
+        pixels.setPixelColor(i, pixels.Color(255, 0, 10));
+      }
+      for(int i = 14; i < 29; i++) {
+        pixels.setPixelColor(i, pixels.Color(0, 10, 255));
+      }
+      for(int i = 29; i < 44; i++) {
+        pixels.setPixelColor(i, pixels.Color(255, 0, 10));
+      }
+    break;
+
+    case 3:
+      for(int i = 0; i < 44; i++) {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+      }
+    break;
+  }
+
+  pixels.show();
+}
+
+void handleSwitchPress() {
+  if(analogRead(ANALOG_SWITCH_PIN) > 100)
+  { 
+    if (switchPressStartTime == 0) {
+      switchPressStartTime = millis();
+    } else {
+      unsigned long currentTime = millis();
+      if (currentTime - switchPressStartTime >= SWITCH_HOLD_TIME) {
+        factoryReset();
+        switchPressStartTime = 0;
+      }
+    }
+
+    delay(45);
+  } else {
+    if (switchPressStartTime != 0) {
+      cycleColors();
+    }
+    
+    switchPressStartTime = 0;
+  }
+
+  delay(100);
+}
+
+void cycleColors() {
+  Serial.println("Cycling colors");
+  LED_COLOR_MODE = LED_COLOR_MODE + 1;
+}
+
+void factoryReset() {
+  Serial.println("Factory Reset");
+  pixels.clear();
+  red1.clear();
+  red2.clear();
+  red3.clear();
+  delay(500);
+  manager.resetSettings();
+  ESP.restart();
+}
